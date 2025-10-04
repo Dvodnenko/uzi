7e9054caf2e25e09b5500b2f624b176fdc7db18f
Dvodnenko <thekovalskii@gmail.com>
2025-10-04 17:43:56 +0300

    feat: add groups update method

--- src/raw/application/services/groups.py
+++ src/raw/application/services/groups.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from ...domain import Group, EntityRepository, Config, UseCaseResponse
 
 
@@ -14,8 +16,14 @@ class GroupService:
         if self.repository.get(group.title):
             return UseCaseResponse(f"Group already exists: {group.title}", status_code=5)
         if not self.repository.get(group.title.parent):
-            return UseCaseResponse(f"Group '{group.title.parent}' does not exist", status_code=4)
-        self.repository.save(group)
+            return UseCaseResponse(f"Group not found: {group.title.parent}", status_code=4)
+        self.repository.create(group)
         return UseCaseResponse(f"Group created: {group.title}")
         
-    
+    def update(self, title: Path, new: Group):
+        if not self.repository.get(title):
+            return UseCaseResponse(f"Group not found: {title}", status_code=4)
+        if self.repository.get(new.title):
+            return UseCaseResponse(f"Group already exists: {new.title}", status_code=5)
+        self.repository.update(title, new)
+        return UseCaseResponse(f"Group updated: {title}")
