d53d3f086f233fdb9dd0bc1cdfe1bba007788d38
Dvodnenko <thekovalskii@gmail.com>
2025-10-26 16:52:28 +0200

    Revert "refactor: change ORM session approach to regular connection"
    This reverts commit 9e0358fc7aed24b54f58844d57ba6fa0f2430aa0.

--- src/raw/rawd/database/funcs.py
+++ src/raw/rawd/database/funcs.py
@@ -1,14 +1,12 @@
 from sqlalchemy import select
 
 from ..entities import Entity
-from .session import engine
 
 
 def get_all_by_titles(
-    model, titles: list[str]
+    session, model, titles: list[str]
 ) -> list[Entity]:
-    with engine.connect() as conn:
         query = select(model) \
             .where(model.title.in_(titles))
-        objs = conn.scalars(query).unique().all()
+        objs = session.scalars(query).unique().all()
         return objs
