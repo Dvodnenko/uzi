894e9e0ace8aafe4d6fcc4104ea8c9b2e889fbda
Dvodnenko <thekovalskii@gmail.com>
2025-10-29 17:26:26 +0200

    refactor: turn tag service methods to generators

--- src/raw/rawd/services/tags.py
+++ src/raw/rawd/services/tags.py
@@ -11,46 +11,44 @@ class TagService:
         self.folders_repository = saFolderRepository(repository.session)
 
     @cast_kwargs(Tag)
-    def create(self, args: list, flags: list, **kwargs) -> tuple[str, int]:
+    def create(self, args: list, flags: list, **kwargs):
         tag = Tag(**kwargs)
-        if self.repository.get(tag.title):
-            return f"Tag already exists: {tag.title}", 1
-        self.repository.create(tag)
-        return f"Tag created: {tag.title}", 0
+        if next(self.repository.get(tag.title)):
+            yield f"Tag already exists: {tag.title}", 1
+            return
+        next(self.repository.create(tag))
+        yield f"Tag created: {tag.title}", 0
     
     @provide_conf
     def all(self, args: list, flags: list, **kwargs):
         sortby = kwargs.get("sortby", "title")
-        tags = self.repository.get_all()
-        tags = sorted(
-            tags,
-            key=lambda t: getattr(t, sortby),
-            reverse="r" in flags
-        )
-        pattern: str = kwargs["__cnf"]["formats"]["tag"]
         if "t" in flags:
-            return "".join(f"{t.title}\n" for t in tags)[:-1], 0
-        return "".join([f"{pattern.format(
-            **t.to_dict())}" for t in tags]).rstrip(), 0
+            for tag in self.repository.get_all(sortby):
+                yield tag.title, 0
+        else:
+            pattern: str = kwargs["__cnf"]["formats"]["tag"]
+            for tag in self.repository.get_all(sortby):
+                yield pattern.format(**tag.to_dict()), 0
     
     @provide_conf
     def print(self, args: list, flags: list, **kwargs):
-        tags = get_all_by_titles(self.repository.session, Tag, args)
         pattern: str = kwargs["__cnf"]["formats"]["tag"]
-        return "".join([f"{pattern.format(
-            **t.to_dict())}" for t in tags]).rstrip(), 0
+        for tag in get_all_by_titles(self.repository.session, Tag, args):
+            yield pattern.format(**tag.to_dict()), 0
     
     @cast_kwargs(Tag)
     def update(self, args: list, flags: list, **kwargs):
-        current = self.repository.get(args[0])
+        current = next(self.repository.get(args[0]))
         if not current:
-            return f"Tag not found: {args[0]}", 1
-        self.repository.update(args[0], **kwargs)
-        return f"Tag updated: {args[0]}", 0
+            yield f"Tag not found: {args[0]}", 1
+            return
+        next(self.repository.update(args[0], **kwargs))
+        yield f"Tag updated: {args[0]}", 0
 
     def delete(self, args: list, flags: list, **kwargs):
-        tag = self.repository.get(args[0])
+        tag = next(self.repository.get(args[0]))
         if not tag:
-            return f"Tag not found: {args[0]}", 1
-        self.repository.delete(tag)
-        return f"Tag deleted: {args[0]}", 0
+            yield f"Tag not found: {args[0]}", 1
+            return
+        next(self.repository.delete(tag))
+        yield f"Tag deleted: {args[0]}", 0
