b10e311e114502147d165a6b6af55971bb243d83
Dvodnenko <thekovalskii@gmail.com>
2025-11-12 14:51:42 +0200

    fix: links parsing

--- src/sine/daemon/services/decorators.py
+++ src/sine/daemon/services/decorators.py
@@ -10,33 +10,36 @@ from ..funcs import is_, cast_datetime
 
 def cast_kwargs(
     model: type[Entity] = Entity,
-    exclude: set[str] = {},
+    exclude: set[str] = {
+        "children", "id", "parent", 
+        "parent_id", "type"},
 ):
 
     def decorator(func):
         def wrap(self, args: list, flags: list, **kwargs):
-            allowed = {f for f in fields(model)}
+            allowed = {f for f in fields(model)}.difference(exclude)
 
             for field in allowed:
                 if not field.name in {f.name for f in allowed}\
                     .intersection(k for k in kwargs.keys())\
-                    .difference(exclude):
+                    .difference({"links"} # links cannot be parsed here
+                ): 
                     continue
                 if is_(field.type, datetime):
                     kwargs[field.name] = cast_datetime(kwargs[field.name])
                 elif is_(field.type, IntEnum):
                     field.type(int(kwargs[field.name]))
 
-            if kwargs.get("links"):
+            if kwargs.get("links") is not None:
                 if kwargs["links"] == "":
-                    ...
+                    kwargs["links"] = []
                 else:
                     linksstr: str = kwargs["links"]
                     kwargs["links"] = []
                     gen = get_all_by_titles(
                         self.repository.session,
                         Entity, 
-                        linksstr.split(",")
+                        linksstr.split(",") if "," in linksstr else [linksstr]
                     )
                     for link in gen:
                         kwargs["links"].append(link)
